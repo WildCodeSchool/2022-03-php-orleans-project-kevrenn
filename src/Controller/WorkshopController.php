0c3c4b40d33998fbd198644bdfdebc612ce7ca45
kylian moreira <kylianmoreira@gmail.com>
2022-04-20 16:58:39 +0200

    style Card atelier

--- src/Controller/WorkshopController.php
+++ src/Controller/WorkshopController.php
@@ -6,13 +6,11 @@ use App\Model\WorkshopManager;
 
 class WorkshopController extends AbstractController
 {
-
     public function index(): string
     {
         $workshopManager = new WorkshopManager();
         $workshops = $workshopManager->selectAll();
 
         return $this->twig->render('Workshop/index.html.twig', ['workshops' => $workshops,]);
-
     }
 }
