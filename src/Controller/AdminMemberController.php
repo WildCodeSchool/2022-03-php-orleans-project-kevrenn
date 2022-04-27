1eaca6f926a9867f79bdf74f74e78be21ae92e83
VanTej <vanessatejedor@gmail.com>
2022-04-27 14:32:01 +0200

    liste des membres triée par nom

--- src/Controller/AdminMemberController.php
+++ src/Controller/AdminMemberController.php
@@ -9,7 +9,7 @@ class AdminMemberController extends AbstractController
     public function index(): string
     {
         $memberManager = new MemberManager();
-        $members = $memberManager->selectAll();
+        $members = $memberManager->selectAll('name');
 
         return $this->twig->render('Admin/Member/index.html.twig', ['members' => $members]);
     }
