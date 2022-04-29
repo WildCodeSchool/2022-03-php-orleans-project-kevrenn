67ba8cb0c1ac73cfac0c89d5e99ca8220e03d3be
VanTej <vanessatejedor@gmail.com>
2022-04-29 09:50:11 +0200

    correction espace manager et ajout controller avec validation form

--- src/Model/MemberManager.php
+++ src/Model/MemberManager.php
@@ -8,7 +8,7 @@ class MemberManager extends AbstractManager
 
     public function update(array $member): bool
     {
-        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `name` = :name, `status` = :status  WHERE id=:id");
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . "SET `name`=:name,`status`=:status  WHERE id=:id");
         $statement->bindValue('id', $member['id'], \PDO::PARAM_INT);
         $statement->bindValue('name', $member['name'], \PDO::PARAM_STR);
         $statement->bindValue('status', $member['status'], \PDO::PARAM_STR);
