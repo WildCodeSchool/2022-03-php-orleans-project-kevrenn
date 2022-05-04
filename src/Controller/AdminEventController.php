68be6eae57df1159fa6acaeddb9822c5b6895be5
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-05-04 10:07:35 +0200

    added code for controler and manager

--- src/Controller/AdminEventController.php
+++ src/Controller/AdminEventController.php
@@ -13,4 +13,25 @@ class AdminEventController extends AbstractController
 
         return $this->twig->render('Admin/Event/index.html.twig', ['events' => $events]);
     }
+
+    public function edit(int $id): ?string
+    {
+        $eventManager = new EventManager();
+        $event = $eventManager->selectOneById($id);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $event = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+            $eventManager->update($event);
+
+            header('Location: /admin/evenements/');
+
+            return null;
+        }
+
+        return $this->twig->render('Admin/Event/_edit.html.twig', [
+            'event' => $event,
+        ]);
+    }
 }
