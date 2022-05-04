0f44a63633c7277f783a8d810a5371af82815e95
VanTej <vanessatejedor@gmail.com>
2022-05-04 15:25:51 +0200

    contrôle + validation + route

--- src/Controller/AdminPartnerController.php
+++ src/Controller/AdminPartnerController.php
@@ -6,11 +6,68 @@ use App\Model\PartnerManager;
 
 class AdminPartnerController extends AbstractController
 {
+    public const BDD_LENGTH = 255;
+    public array $errors = [];
+
     public function index(): string
     {
         $partnerManager = new PartnerManager();
         $partners = $partnerManager->selectAll('name');
 
         return $this->twig->render('Admin/Partner/index.html.twig', ['partners' => $partners]);
+    }
+
+    public function add(): ?string
+    {
+        $partner = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $partner = array_map('trim', $_POST);
+
+            $this->validate($partner);
+            if (!filter_var($partner['url'], FILTER_VALIDATE_URL)) {
+                $this->errors[] = 'L\'adresse du site est invalide';
+            }
+
+            if (empty($this->errors)) {
+                $partnerManager = new PartnerManager();
+                $partnerManager->insert($partner);
+                header('Location: /admin/partenaires');
+            }
+        }
+
+        return $this->twig->render('Admin/Partner/add.html.twig', [
+            'partner' => $partner,
+            'errors' => $this->getErrors(),
+        ]);
+    }
+
+    private function isEmpty(string $label, $input): void
+    {
+        if (empty($input)) {
+            $this->errors[] = "$label ne peut pas être vide";
+        }
+    }
+
+    private function isTooLong(string $label, $input, int $const): void
+    {
+        if (strlen($input) > $const) {
+            $this->errors[] = "$label ne peut pas être plus long que $const caractères";
+        }
+    }
+
+    private function validate($partner): void
+    {
+        $this->isEmpty('Nom', $partner['name']);
+        $this->isEmpty('Adresse du site', $partner['url']);
+        $this->isEmpty('Lien du logo', $partner['logo_link']);
+        $this->isTooLong('Nom', $partner['name'], self::BDD_LENGTH);
+        $this->isTooLong('Adresse du site', $partner['url'], self::BDD_LENGTH);
+        $this->isTooLong('Lien du logo', $partner['logo_link'], self::BDD_LENGTH);
+    }
+
+
+    public function getErrors(): array
+    {
+        return $this->errors;
     }
 }
