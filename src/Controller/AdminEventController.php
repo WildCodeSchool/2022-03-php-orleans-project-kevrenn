8bbe6543e12cf432720564e82a4fd703b4022711
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-05-04 02:09:44 +0200

    added delete function

--- src/Controller/AdminEventController.php
+++ src/Controller/AdminEventController.php
@@ -42,4 +42,15 @@ class AdminEventController extends AbstractController
             'event' => $event,
         ]);
     }
+
+    public function delete(): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = trim($_POST['id']);
+            $eventManager = new EventManager();
+            $eventManager->delete((int)$id);
+
+            header('Location:/admin/evenements');
+        }
+    }
 }
