ee3be8e490520d11c63d3aa405c30c4c44aac13e
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-05-04 12:11:59 +0200

    added the security on the form

--- src/Controller/AdminEventController.php
+++ src/Controller/AdminEventController.php
@@ -6,6 +6,38 @@ use App\Model\EventManager;
 
 class AdminEventController extends AbstractController
 {
+    public const NAME_LENGTH = 255;
+    public const ADRESS_LENGTH = 255;
+    public const IMAGE_LINK_LENGTH = 255;
+
+    public array $errors = [];
+
+    private function isEmpty(string $label, $input): void
+    {
+        if (empty($input)) {
+            $this->errors[] = "$label ne peut pas être vide";
+        }
+    }
+
+    private function isTooLong(string $label, $input, int $const): void
+    {
+        if (strlen($input) > $const) {
+            $this->errors[] = "$label ne peut pas être plus long que $const caractères";
+        }
+    }
+
+    private function validate($event): void
+    {
+        $this->isEmpty('Nom', $event['name']);
+        $this->isEmpty('Date', $event['date']);
+        $this->isEmpty('Description', $event['description']);
+        $this->isEmpty('Adresse', $event['address']);
+        $this->isEmpty('Image', $event['image_link']);
+        $this->isTooLong('Nom', $event['name'], self::NAME_LENGTH);
+        $this->isTooLong('Adresse', $event['address'], self::ADRESS_LENGTH);
+        $this->isTooLong('Image', $event['image_link'], self::IMAGE_LINK_LENGTH);
+    }
+
     public function index(): string
     {
         $eventManager = new EventManager();
@@ -22,16 +54,22 @@ class AdminEventController extends AbstractController
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $event = array_map('trim', $_POST);
 
-            // TODO validations (length, format...)
-            $eventManager->update($event);
+            $this->validate($event);
 
-            header('Location: /admin/evenements/');
-
-            return null;
+            if (empty($this->errors)) {
+                $eventManager->update($event);
+                header('Location: /admin/evenements/');
+            }
+            
         }
 
         return $this->twig->render('Admin/Event/_edit.html.twig', [
             'event' => $event,
+            'errors' => $this->getErrors(),
         ]);
+    }
+    public function getErrors(): array
+    {
+        return $this->errors;
     }
 }
